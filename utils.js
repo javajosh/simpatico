410398f7bb341ca8a3ad0fe6405ba58b75f73df6
javajosh <javajosh@gmail.com>
2019-01-20 19:48:51 +0100

    more major refactorings

--- utils.js
+++ utils.js
@@ -6,87 +6,106 @@
  */
 
 const TYPES = {
-  STR : 'string', 
+  STR: 'string',
   ANY: '',
-  NUM: 'number', 
+  NUM: 'number',
   BOOL: 'boolean',
   OBJ: 'object',
   FUN: 'function',
   ARR: 'array',
   NULL: 'null',
-  UNDEF : 'undefined',
-  CALL : 'call',
+  UNDEF: 'undefined',
+  CALL: 'call',
   HANDLER: 'handler',
 };
 
-// Object.keys(TYPES).forEach(key => window[key] = TYPES[key]);
-
-const types = Object.keys(TYPES);
-
-const assert = (a, msg) => {if (!a) throw new Error(msg)};
-const assertEquals = (expected, actual) => {if (actual !== expected) assert(false, `expected [${expected}] but got [${actual}]`)};
+const assert = (a, msg) => {
+  if (!a) throw new Error(msg)
+};
+const assertEquals = (expected, actual) => {
+  if (actual !== expected) assert(false, `expected [${expected}] but got [${actual}]`)
+};
 
 const assertType = (type, a) => assert(getType(a) === type, `Expected ${a} to be of type ${type} but was of type ${getType(a)}`);
 
 const assertThrows = fn => {
+  assertType(TYPES.FUN, fn);
   let throws = false;
-  try{
+  try {
     fn();
-  }catch (e){
+  } catch (e) {
     throws = true;
   }
-  if (!throws) throw new Error(`Expected fn to throw but it didn't`);
+  assert(throws, `Expected fn to throw, but it didn't`);
 };
+
+// This is a very simple curry for two variables. Pareto principal!
+const curry = (f, a) => b => f(a, b);
+
+// Map over all entries passing (key,val) into the mapFn
+const objectMap = (object, mapFn) => Object.keys(object).reduce(
+  (result, key) => {
+    result[key] = mapFn(key, object[key]);
+    return result;
+  }, {});
 
 
 const getType = (a) => {
   let t = typeof a;
   if (t !== TYPES.OBJ) return t;
-  
+
   if (Array.isArray(a)) return TYPES.ARR;
   if (a === null) return TYPES.NULL;
   if (a === undefined) return TYPES.UNDEF;
-  
+
   if (a.hasOwnProperty(TYPES.CALL)) return TYPES.CALL;
   if (a.hasOwnProperty(TYPES.HANDLER)) return TYPES.HANDLER;
-  
+
   return TYPES.OBJ;
+};
+
+// Cast a string to a particular type
+const cast = (type, str) => {
+  switch (type) {
+    case TYPES.STRING:
+      return str;
+    case TYPES.NUM:
+      const result = 1 * str;
+      if (Number.isNaN(result)) throw new Error(`Cannot convert ${str} into a number`);
+      return result;
+    case TYPES.BOOL:
+      return (str === 'true') ? true : false;
+  }
 };
 
 const predicates = {
   get: (arr, pos) => pos.length ? get(arr[pos.shift()], pos) : arr,
-  all: (arr, val) =>  arr.reduce((acc,a) => (a === val) && acc, true),
+  all: (arr, val) => arr.reduce((acc, a) => (a === val) && acc, true),
   same: (arr) => predicates.all(arr, arr[0]),
-  eq : (a, b) => a.length === b.length && predicates.all(zip(a,b).map(d => d[0] === d[1])),
-  contains : (arr, a) => arr.contains(a),
-  between :(min, max, num) => min <= num && num <=max,
-}
-
+  eq: (a, b) => a.length === b.length && predicates.all(zip(a, b).map(d => d[0] === d[1])),
+  contains: (arr, a) => arr.contains(a),
+  between: (min, max, num) => min <= num && num <= max,
+  deepEquals : (a,b) => JSON.stringify(a) === JSON.stringify(b),
+};
 
 // Generally useful functions on arrays.
-const zip = (a, b) => a.map((d, i) => [d, b[i]]);
-const add = (a, b) => zip(a,b).map(d => d[0] + d[1]);
-const dot = (a, b) => zip(a,b).map(c => d[0] * d[1]);
-const mul = (arr, c) => arr.map(d => c * d);
-
-// This is a very simple curry for two variables. Pareto principal!
-const curry = (f, a) => b => f(a,b);
-
-// Map over all entries passing (key,val) into the mapFn
-const objectMap = (object, mapFn) => {
-    return Object.keys(object).reduce((result, key) =>{
-        result[key] = mapFn(key, object[key])
-        return result
-    }, {})
-}
+const arrays = {
+  zip: (a, b) => a.map((d, i) => [d, b[i]]),
+  add: (a, b) => arrays.zip(a, b).map(d => d[0] + d[1]),
+  dot: (a, b) => arrays.zip(a, b).map(c => d[0] * d[1]),
+  mul: (arr, c) => arr.map(d => c * d),
+  _f : (a, b) => [].concat(...a.map(d => b.map(e => [].concat(d, e)))),
+  cartesian : (a, b, ...c) => (b ? arrays.cartesian(arrays._f(a, b), ...c) : a),
+};
 
 
 const assertions = {
-  all: (arr,val) => assert(predicates.all(arr, val), `Expected all array values [${arr}] to be ${val} but they weren't`),
+  all: (arr, val) => assert(predicates.all(arr, val), `Expected all array values [${arr}] to be ${val} but they weren't`),
   same: (arr) => assert(predicates.same(arr), `Expected all values in array [${arr}] to be the same, but they weren't`),
-  eq: (a, b) => assert(predicates.eq(a,b), `Expected arrays to be equal [${a}] and [${b}]`),
+  eq: (a, b) => assert(predicates.eq(a, b), `Expected arrays to be equal [${a}] and [${b}]`),
   contains: (arr, a) => assert(predicates.contains(arr, a), `Expected array [${arr}] to contain value ${a}, but it didn't`),
-  between: (min, max, num) => assert(predicates.between(min, max, num),`Middle number out of bounds: ${min} <= ${num} && ${num} <= ${max}`),
+  between: (min, max, num) => assert(predicates.between(min, max, num), `Middle number out of bounds: ${min} <= ${num} && ${num} <= ${max}`),
+  deepEquals: (a,b) => assert(predicates.deepEquals(a,b), `${a} is not deeply equal to ${b}`),
 }
 
 // Add all the types to the assertion object too.
@@ -94,10 +113,46 @@ objectMap(TYPES, (typeHandle, type) => assertions[typeHandle.toLowerCase()] = cu
 
 // This is basically an object.assign that prints out an error if the target doesn't permit setting the key
 const install = (target, source) => Object.keys(source).forEach(key => {
-  try{
+  try {
     target[key] = source[key];
   } catch (e) {
     console.error(`Tried to install ${key} but an exception was thrown ${e.message}`);
   }
 });
 
+
+// Combine two objects, mutating target
+// Similar to Object.assign, but use the target values
+// (The only time we don't use the value is when the target is a string)
+const combine = (target, obj, print=false) => {
+  if (typeof obj === 'undefined') return;
+  for (const key in obj){
+    if (typeof target[key] === 'boolean'){
+      target[key] = !target[key];
+    } else if (typeof target[key] === 'number'){
+      target[key] = target[key] + obj[key];
+    } else {
+      target[key] = obj[key]
+    }
+  }
+  if (print) console.log(target, obj);
+}
+
+// Gather data from the (SVG) DOM into obj, casting them according to initial values in obj.
+const gather = (elt, obj) => {
+  for (const key in obj){
+    if (!elt.hasAttribute(key)) continue;
+    const val = elt.getAttribute(key);
+    const type = getType(obj[key]);
+    obj[key] = cast(type, val);
+  }
+};
+
+// Scatter data from an obj onto the (SVG) DOM, writing only if the values differ.
+const scatter = (elt, obj) => {
+  for (const key in obj){
+    const old = elt.getAttribute(key);
+    if (obj[key] + '' !== old)
+      elt.setAttribute(key, obj[key]);
+  }
+}
