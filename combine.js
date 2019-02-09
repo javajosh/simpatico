d6b309f49b7c46cc49974e4aca84803257215665
Josh Rehman <josh@joshrehman.com>
2019-02-09 17:00:48 +0100

    Cleanup combine; make null targetable.

--- combine.js
+++ combine.js
@@ -8,15 +8,12 @@ const combine = (target, msg, print=false) => {
   let ttarget = getType(target);
   let tmsg = getType(msg);
 
-  // Rules about function application are too hard to encode as a simple object,
-  // So we implement this (limited) functionality here as "virtual rules".
-  // Unless null is involved (which often has a special meaning):
-  // Virtual rule #1: A function message is applied to the target, and vv.
-  // Virtual rule #2: A message to an array that isn't an array gets pushed
-  if (tmsg !== NULL && ttarget !== NULL){
-    if      (tmsg === FUN && ttarget !== FUN) ttarget = ANY;
-    else if (ttarget === FUN && tmsg !== FUN) tmsg    = ANY;
-    else if (ttarget === ARR && tmsg !== ARR) tmsg    = ANY;
+  if (ttarget == NULL){
+    tmsg = ANY;
+  } else if (tmsg !== NULL){
+    if      (tmsg === FUN) ttarget = ANY;
+    else if (ttarget === FUN) tmsg = ANY;
+    else if (ttarget === ARR && tmsg !== ARR) tmsg = ANY;
   }
 
   const ruleKey = ttarget + tmsg;
@@ -30,31 +27,28 @@ const combine = (target, msg, print=false) => {
 //Rules for combining things. The first arg is the target
 //We proceed from the "natural" types to the synthetic types introduced by Simpatico.
 const rules = {};
-rules[STR+STR] = (_,b) => b;
-rules[STR+NULL] = () => '';
+rules[NULL+ANY] = (_,b) => b;
 
-rules[NUM+NUM]  = (a, b) => a+b;
-rules[NUM+STR]  = (_, b) => cast(TYPES.NUM, b);
-rules[NUM+NULL] = ()     => 0;
+rules[STR+STR]  = (_,b) => b;
+rules[STR+NULL] = ()    => '';
 
-rules[BOOL+BOOL] = (a,b) => b;
-rules[BOOL+STR]  = (_,b) => cast(TYPES.BOOL, b);
-rules[BOOL+NULL] = (a,_) => !a;
+rules[NUM+NUM]  = (a,b) => a + b;
+rules[NUM+STR]  = (_,b) => cast(TYPES.NUM, b);
+rules[NUM+NULL] = ()    => 0;
+
+rules[BOOL+BOOL]= (a,b) => b;
+rules[BOOL+STR] = (_,b) => cast(TYPES.BOOL, b);
+rules[BOOL+NULL]= (a,_) => !a;
 
 rules[ARR+ARR]  = (a,b) => a.concat(b);
 rules[ARR+ANY]  = (a,b) => {a.push(b); return a;};
 rules[ARR+NULL] = ()    => [];
 
+rules[FUN+ANY]  = (a,b) => a(b);
+rules[ANY+FUN]  = (a,b) => b(a);
 
-rules[FUN+ANY]  = (fn, b) => fn(b);
-rules[ANY+FUN]  = (a, fn) => fn(a);
-
-rules[NULL+FUN] = (_,fn)  => fn;
-
-
-
-rules[OBJ+OBJ] = (a,b) => {
-  b = Object.assign({},b);
+rules[OBJ+OBJ]  = (a,b) => {
+  const b = Object.assign({},b);
   for (let prop in a){
     b[prop] = b.hasOwnProperty(prop) ?
       combine(a[prop],b[prop]): //recurse
@@ -76,6 +70,7 @@ rules[OBJ+MSG] = (core, msg) => { //handler invocation
 
   return core;
 };
+
 rules[OBJ+HANDLER] = (core, handler) => { //handler registration
   core = combine(core, {handlers:{}});
   core.handlers[handler.name] = handler;
