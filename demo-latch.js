6d5221b7b2b6c6cbdbe395d4417e0ce785c1d914
Josh Rehman <josh@joshrehman.com>
2021-07-05 16:55:45 -0400

    Increase the size of valid decrements and fix up imports in demo-latch.js Improve tests in rtree.html

--- demo-latch.js
+++ demo-latch.js
@@ -1,9 +1,7 @@
-import Core from './core.js';
-
-import rtree from './rtree.js';
+import {tryToStringify, hasProp} from './core.js';
 import {validate} from './friendly.js'
+import rtree from './rtree.js';
 
-const {tryToStringify, hasProp} = Core;
 
 // Let's build a little countdown latch
 const latch = rtree({count: 100});
@@ -11,19 +9,20 @@ const latch = rtree({count: 100});
 latch.add({
   name: 'dec',
   desc: 'A counter that goes to 0 in different intervals',
-  pattern: {amount:['optional', 'num', 'between', 0, 10]},
+  pattern: {msg: 'dec', amount:['optional', 'num', 'between', 0, 100]},
   example: {msg: 'dec', amount: 5},
   dec: {count: -1},
-  handle: function(ctx, msg){ // 'function' is the sad syntactical price of 'this'
+  handle: function(ctx, msg){ // 'function' is the price of using 'this'
     const {pattern, dec} = this;
 
     if (ctx.count <= 0) throw `halted because count ${ctx.count} <=0`;
-    if (ctx.err)        throw `halted with error ${tryToStringify(ctx.err.failures)}`;
+    if (ctx.err)        throw `halted with error ${tryToStringify(ctx.err.failures)} for msg ${tryToStringify(ctx.err.msg)}`;
 
     const failures = validate(pattern, msg);
 
-    if (failures) return {err: {msg, failures}}
+    if (failures) return {err: {msg, failures}};
     if (hasProp(msg, 'amount')) return {count: -msg.amount};
+
     return dec;
   }
 });
