692186c5653e77cdce587406a584391175d59dd3
JavaJosh <github@javajosh.com>
2023-01-20 20:25:20 -0500

    log the remote ip address, not our own!

--- reflector.js
+++ reflector.js
@@ -29,7 +29,7 @@ console.log("UTC", new Date().toUTCString(), process.cwd(), args);
 
 // Http file server
 http.createServer((req, res) => {
-  console.log(req.socket.address(), req.url, req.headers["user-agent"]);
+  console.log(req.socket.remoteAddress, req.url, req.headers["user-agent"].substr(0,15));
   if (req.url === '/') req.url = '/index.html';
   fs.readFile(process.cwd() + req.url, (err, data) => {
     if (err) {
