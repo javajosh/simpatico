d2e00d3b107b4ff5f160e4a414b218f5bc6c18f0
JavaJosh <github@javajosh.com>
2023-04-07 19:58:04 -0400

    reflector: not sure why this became necessary but needed more CSP permissions

--- reflector.js
+++ reflector.js
@@ -189,7 +189,7 @@ function fileServerLogic() {
   const getContentSecurityPolicyHeaders = () => ({
     'Content-Security-Policy': [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-inline'",
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
       "style-src  'self' 'unsafe-inline'",
       "img-src 'self' data:"
     ].join(';')
