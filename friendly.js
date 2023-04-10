ab7aa8c9232ed1e2cdaf4619c2a0db67aa3c7095
javajosh <javajosh@gmail.com>
2023-04-10 19:42:15 -0400

    friendly: failed predicates now include the value that made them fail.

--- friendly.js
+++ friendly.js
@@ -15,7 +15,7 @@ import { assert, is, tryToStringify, size, hasProp } from "./core.js";
  * @param valueObj
  * @returns {{}|undefined|*} undefined if the object passes; an object describing the failure if it fails.
  */
-export const validate = (patternObj, valueObj) => {
+const validate = (patternObj, valueObj) => {
   //gotcha: do not use getType predicate because handler !== object, etc
   const isObjecty = a => is.obj(a) || is.msg(a)
   if (!isObjecty(valueObj)) return patternObj;
@@ -51,11 +51,11 @@ export const validate = (patternObj, valueObj) => {
  * This is an internal method exported only to support testing. but it checks a value against an array of predicates.
  * This function is the one you want to change if you want to add new predicates or features to the system.
  *
- * @param predArray
- * @param value
- * @returns {undefined|*} Undefined (falsy) if pass - like the Unix convention. If there's a problem, return it.
+ * @param predArray An array describing the valid value
+ * @param value The value to be checked
+ * @returns {undefined|*} Undefined (falsy) if pass. If there's a problem, return an array of failed predicates.
  */
-export const checkValue = (predArray, value) => {
+const checkValue = (predArray, value) => {
   if (!is.arr(predArray)){
     return value === predArray ? undefined : ["equals", predArray];
   }
@@ -64,39 +64,55 @@ export const checkValue = (predArray, value) => {
   let pass = true;
   let allPass = true;
 
-  // Loop through the predicates.
   for (let i = 0; i < predArray.length; i++) {
-    let pred = predArray[i].toLowerCase(); // case of preds doesn't matter
+    let pred = predArray[i].toLowerCase();
+    if (pred === 'failed') break;
+    switch (pred) {
+      case "between":
+        const lo = predArray[i + 1];
+        const hi = predArray[i + 2];
+        i += 2;
 
-    if (pred === "between") {
-      const lo = predArray[i + 1];
-      const hi = predArray[i + 2];
-      pass = is.between(lo, hi, size(value));
-      i += 2;
-      if (!pass) {
-        failedPreds.push(pred, lo, hi);
-      }
-    } else if (pred === "pick") {
-      const count = predArray[i + 1];
-      const options = predArray.slice(i + 2);
-      i += 1 + options.length;
+        pass = is.between(lo, hi, size(value));
+        if (!pass) {
+          predArray.push('failed')
+          predArray.push(value);
+          failedPreds.push(pred, lo, hi);
+        }
+        break;
+      case "pick":
+        const count = predArray[i + 1];
+        const options = predArray.slice(i + 2);
+        i += 1 + options.length;
 
-      pass = options.includes(value);
-      if (!pass) {
-        failedPreds.push(pred, count, options);
-      }
-    } else if (pred === "optional") {
-      //optional means we skip if it's missing
-      if (is.undef(value)) {
-        return undefined;
-      }
-    } else {
-      // You can call any predicate by name - typically this will be a type check
-      if (!hasProp(is, pred)) throw `missing predicate ${pred}`
-      pass = is[pred](value);
-      if (!pass) failedPreds.push(pred);
+        pass = options.includes(value);
+        if (!pass) {
+          predArray.push('failed')
+          predArray.push(value);
+          failedPreds.push(pred, count, options);
+        }
+        break;
+      case "optional":
+        pass = true;
+        if (is.undef(value)) {
+          return undefined;
+        }
+        break;
+      default:
+        // You can call any predicate by name - typically this will be a type check
+        if (!hasProp(is, pred)) {
+          predArray.push('failed');
+          throw `missing predicate ${pred}`;
+        }
+        pass = is[pred](value);
+        if (!pass) {
+          failedPreds.push(pred);
+        }
+        break;
     }
     allPass = allPass && pass;
   }
   return allPass ? undefined : failedPreds;
 };
+
+export {validate, checkValue}
