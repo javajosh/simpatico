fdea0af9bf93c7f9017ce1512eb376210033b63f
JavaJosh <github@javajosh.com>
2023-08-23 21:55:31 -0400

    kata/java: vanilla and spring boot templates.

---
+++ kata/java/spring.exercises/spring.exercises/src/test/java/io/simpatico/java/spring/exercises/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.simpatico.java.spring.exercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
