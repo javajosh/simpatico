28d04dcc0dd0a4cd349b48b4d0faa9595441d437
JavaJosh <github@javajosh.com>
2023-09-28 16:19:20 -0400

    react: play around with react and vite

--- kata/vite-react/src/App.jsx
+++ kata/vite-react/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+function App({name}) {
   const [count, setCount] = useState(0)
 
   return (
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + {name}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
