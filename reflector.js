70430881ba882ba9f160739b794efcc9f2a46f70
JavaJosh <github@javajosh.com>
2024-02-26 16:59:13 -0500

    site: add rss+xml mime type

--- reflector.js
+++ reflector.js
@@ -212,6 +212,7 @@ function fileServerLogic() {
     "jpg" : "image/jpeg",
     "jpeg": "image/jpeg",
     "woff2": "font/woff2", // see https://github.com/w3c/woff
+    "xml": "application/rss+xml",
   }
   /**
    * Required for most browsers to use SharedArrayBuffer and load wasm.
